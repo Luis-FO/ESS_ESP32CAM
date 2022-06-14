10d4ab7ae3a9838e32b3a55c44cf3e5a047fe7f6
Luís Fernando de Oliveira <luisfernandophsc@gmail.com>
2022-06-14 16:36:07 -0300

    File update

--- poc/Esp_devkitv1_ard/examples/mqtt_teste.cpp
+++ poc/Esp_devkitv1_ard/examples/mqtt_teste.cpp
@@ -1,12 +1,3 @@
-/* MQTT (over TCP) Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include <Arduino.h>
 #include <WiFi.h>
 
@@ -38,82 +29,57 @@ static const char *TAG = "MQTT_EXAMPLE";
 static void log_error_if_nonzero(const char *message, int error_code)
 {
     if (error_code != 0) {
-        // ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
         printf("Last error %s: 0x%x\n", message, error_code);
     }
 }
 
-/*
- * @brief Event handler registered to receive MQTT events
- *
- *  This function is called by the MQTT client event loop.
- *
- * @param handler_args user data registered to the event.
- * @param base Event base for the handler(always MQTT Base in this example).
- * @param event_id The id for the received event.
- * @param event_data The data for the event, esp_mqtt_event_handle_t.
- */
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
-    // ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
     printf("Event dispatched from event loop base=%s, event_id=%d\n", base, event_id);
     esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)event_data;
     esp_mqtt_client_handle_t client = event->client;
     int msg_id;
     switch ((esp_mqtt_event_id_t)event_id) {
     case MQTT_EVENT_CONNECTED:
-        // ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
         printf("MQTT_EVENT_CONNECTED\n");
         msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
-        //ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
         printf("sent publish successful, msg_id=%d\n", msg_id);
 
         msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
-        //ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
         printf("sent subscribe successful, msg_id=%d\n", msg_id);
 
         msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-        //ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
         printf("sent subscribe successful, msg_id=%d\n", msg_id);
 
         msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-        //ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
         printf("sent unsubscribe successful, msg_id=%d\n", msg_id);
         break;
     case MQTT_EVENT_DISCONNECTED:
         printf("MQTT_EVENT_DISCONNECTED\n");
-        //ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
         break;
 
     case MQTT_EVENT_SUBSCRIBED:
         printf("MQTT_EVENT_SUBSCRIBED, msg_id=%d\n", event->msg_id);
-        //ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
         msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
         printf("sent publish successful, msg_id=%d\n", msg_id);
-        //ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
         break;
     case MQTT_EVENT_UNSUBSCRIBED:
-        //ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
         printf("MQTT_EVENT_UNSUBSCRIBED, msg_id=%d\n", event->msg_id);
         break;
     case MQTT_EVENT_PUBLISHED:
-        //ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED", msg_id=%d", event->msg_id);
         printf("MQTT_EVENT_PUBLISHED, msg_id=%d\n", event->msg_id);
         break;
     case MQTT_EVENT_DATA:
-        // ESP_LOGI(TAG, "MQTT_EVENT_DATA");
         printf("MQTT_EVENT_DATA\n");
         printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
         printf("DATA=%.*s\r\n", event->data_len, event->data);
         break;
     case MQTT_EVENT_ERROR:
-        // ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
         printf("MQTT_EVENT_ERROR\n");
         if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
             log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
             log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
             log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
-            //ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
             printf("Last errno string (%s)\n", strerror(event->error_handle->esp_transport_sock_errno));
             printf("Other event id:%d\n", event->event_id);
 
@@ -121,7 +87,6 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         break;
     default:
         printf("Other event id:%d\n", event->event_id);
-        //ESP_LOGI(TAG, "Other event id:%d", event->event_id);
         break;
     }
 }
