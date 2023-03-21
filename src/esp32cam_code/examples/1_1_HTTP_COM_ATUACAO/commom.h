caebd0c968f03111f6e02dc81fed03a1a006c9cd
LuisF <luisfernandophsc@gmail.com>
2023-03-21 16:38:29 -0300

    Update V2 HTTP SEM ATUACAO

---
+++ src/esp32cam_code/examples/1_1_HTTP_COM_ATUACAO/commom.h
@@ -0,0 +1,80 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "esp_log.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_tls.h"
+#include "esp_http_client.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+
+#define CAMERA_MODEL_AI_THINKER
+
+#if defined(CAMERA_MODEL_WROVER_KIT)
+#define PWDN_GPIO_NUM    -1
+#define RESET_GPIO_NUM   -1
+#define XCLK_GPIO_NUM    21
+#define SIOD_GPIO_NUM    26
+#define SIOC_GPIO_NUM    27
+
+#define Y9_GPIO_NUM      35
+#define Y8_GPIO_NUM      34
+#define Y7_GPIO_NUM      39
+#define Y6_GPIO_NUM      36
+#define Y5_GPIO_NUM      19
+#define Y4_GPIO_NUM      18
+#define Y3_GPIO_NUM       5
+#define Y2_GPIO_NUM       4
+#define VSYNC_GPIO_NUM   25
+#define HREF_GPIO_NUM    23
+#define PCLK_GPIO_NUM    22
+
+#elif defined(CAMERA_MODEL_AI_THINKER)
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+#endif
+
+typedef struct
+{
+  int index;
+  int value;
+}Indexed_Data;
+
+typedef struct {
+  uint8_t *buf;          
+  size_t len;              
+}img_data;
+
+
+int SerialRead(Indexed_Data *values);
+static void init_cam(int aec_value, int agc_gain, framesize_t framesize);
+void start_wifi();
+void capture(void *paremeter);
+void send(void *parameter);
+void configure_pins();
+static void IRAM_ATTR isr(void* arg);
+
